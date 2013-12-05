da0d643d3ee517a780c8ffdc49b19d86e25e7dcc
Nic Hodges <dnhodges@gmail.com>
2013-12-05 11:30:20 -0500

    Bitfield + data file for pieces downloaded.

--- client.rb
+++ client.rb
@@ -10,6 +10,7 @@ require "./peer.rb"
 
 $version = "HT0002"
 $my_id = "-DS00011234567890123"
+$data_file = ""
 
 def print_peer_list(peers)
   list = peers.unpack("C*")
@@ -20,6 +21,39 @@ def print_peer_list(peers)
     x |= slice[5]
     printf("%d.%d.%d.%d:%d\n", slice[0], slice[1], slice[2], slice[3], x)
   end
+end
+
+#writing the data to this file
+def make_data_file 
+
+    data_file = Hash.new
+
+    if File.exist?($data_file) #if a data file exists...
+        
+        encoded =  File.open($data_file, "rb").read.strip
+        data_file = BEncode.load(encoded)
+        $my_id = data_file['my_id']
+    elsif $data_file == "data.dat" # if not, and there's no user supplied string
+        puts "Default data file not found."
+        data_file['my_id'] =  $my_id
+        save $data_file
+    else
+        abort("Error: file not found.")
+    end
+
+    data_file
+end
+
+#save the data file and bencode it
+def save data_file 
+        File.open($data_file, "wb") do |f|
+            f.write(data_file.bencode + "\n")
+        end
+end
+
+#update the data file
+def update data_file, torrent
+    data_file[torrent.info_hash] = torrent.bitfield
 end
 
 #for debugging
@@ -51,7 +85,7 @@ end
 
 
 def usage
-  puts "Usage: \"ruby %s <torrent-file>\"" % [$PROGRAM_NAME]
+  puts "Usage: \"ruby %s <torrent-file> <data-file\"" % [$PROGRAM_NAME]
     puts "\tby default, config is assumed to be in ./.config"
 end
 
@@ -62,12 +96,41 @@ if __FILE__ == $PROGRAM_NAME
     usage
     exit
   when 1
-    file = ARGV[0]
+    torrent_file = ARGV[0]
+    $data_file = "data.dat"
+  when 2
+    torrent_file = ARGV[0]
+    $data_file = ARGV[1]
   end
 
-  torrent = Torrent.new(file)
 
-  print_metadata(torrent)
+  torrent = Torrent.new(torrent_file)
+
+  puts "Storing data in #{$data_file}.\n"
+
+  #print_metadata(torrent) #debug
+
+  data_file = make_data_file
+
+  #setting up the bitfield that represents which pieces have been downloaded successfully  
+  if data_file.has_key? torrent.info_hash
+      puts "Torrent found."
+      torrent.bitfield = data_file[torrent.info_hash] 
+  else
+      puts "Adding torrent to data_file"
+      bitfield_length = torrent.decoded_data["info"]["pieces"].length / 20
+      if bitfield_length % 8 != 0
+          bitfield_length = bitfield_length / 8
+          bitfield_length += 1 
+      else
+          bitfield_length = bitfield_length / 8
+      end
+      torrent.bitfield = "\x0" * bitfield_length
+      puts torrent.bitfield.length
+      puts "Bitfield: #{torrent.bitfield}"
+      update data_file, torrent  
+      save data_file
+  end
 
   # initialize a Tracker object
   options = {:timeout => 5, :peer_id => $my_id}
