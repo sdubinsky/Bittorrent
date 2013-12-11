ea9604ca2d17d9d14172e574ac5a110fbb0f57df
Scott Dubinsky <smdubinsky@gmail.com>
2013-12-10 21:19:30 -0500

    Adds a data read field to the piece class.

--- piece.rb
+++ piece.rb
@@ -1,6 +1,6 @@
 class Piece
-	attr_accessor :frequency, :have, :data, :hash, :number
-	def initialize hash, number
+	attr_accessor :frequency, :have, :hash, :number, :size
+	def initialize hash, number, size
 		#20-byte SHA hash
 		@hash = hash
 		@frequency = 0
@@ -8,10 +8,17 @@ class Piece
 		@number = number
 		@data = nil
 		@lock = Mutex.new
+		#size because it makes it easier to split into blocks and not all pieces are the same size
+		@size = size
 	end
+
 	def set_data data
 		@lock.synchronize {
 			@data = data
 		}
+	end
+
+	def data
+		@data
 	end
 end
