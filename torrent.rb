7e362a4b66c103ffed19e09f4ffa9a95e3d6ab4d
Scott Dubinsky <smdubinsky@gmail.com>
2013-12-13 12:16:50 -0500

    Urlencodes the info_hash

--- torrent.rb
+++ torrent.rb
@@ -11,7 +11,7 @@ class Torrent
   def initialize(filepath)
     @torrent_file = File.read(filepath)
     @decoded_data = BEncode.load(torrent_file)
-    @info_hash = Digest::SHA1.digest(@decoded_data["info"].bencode)
+    @info_hash = CGI.escape(Digest::SHA1.digest(@decoded_data["info"].bencode)).gsub("+", "%20")
     #Hash of peer ids to peers
     @peers = { }
     @piece_directory = FileUtils.mkdir_p(Dir.pwd << "/temp/" << filepath.gsub("/", ""))[0]
